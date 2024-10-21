570f543568f401d1c90b3d6cbeb07a8c256c8c4a
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-10-21 16:15:09 +0300

    Initial Commit

---
+++ HW3_ReferenceTests/HW3_ReferenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_ReferenceTests.swift
+//  HW3_ReferenceTests
+//
+//  Created by Владимир Мацнев on 21.10.2024.
+//
+
+import Testing
+@testable import HW3_Reference
+
+struct HW3_ReferenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
